5fac08353dda94b7a95c1ef0d3359a046854464e
Your NameJeruH_Dev <efiakedohogideon@gmail.com>
2026-09-16 15:59:15 +0100

    Default login and password set for easy access durig development

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,9 @@ import { defineConfig, env } from "prisma/config";
 loadEnvFile();
 
 export default defineConfig({
+  migrations: {
+    seed: "npx tsx prisma/seed.ts",
+  },
   datasource: {
     url: env("DATABASE_URL"),
   },
